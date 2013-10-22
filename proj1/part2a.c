2c66da04e2693cdda778fc4934374ea332980d09
Preston Maness <aggroskater@gmail.com>
2013-10-22 18:17:50 -0500

    Still fiddling with part2a.c; not working yet.

---
+++ proj1/part2a.c
@@ -0,0 +1,94 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Constants as defined in the Project Description */
+#define M 3
+#define K 2
+#define N 3
+#define NUM_THREADS M*N
+
+int A[M][K] = {{1,4},{2,5},{3,6}};
+int B[K][N] = {{8,7,6},{5,4,3}};
+int C[M][N] = {0}; /* Need to initialize to zero for this to work */
+
+/* Thread argument struct */
+struct thread_args {
+
+  int thread_id;
+  int row;
+  int column;
+
+};
+
+/* Threaded code */
+void *matrixCruncher(void* threadArgs) {
+
+  struct thread_args *local;
+  local = (struct thread_args *) threadArgs;
+
+  int row, column;
+  row = local->row;
+  column = local->column; 
+
+  printf("In thread %d crunching C[%d][%d].\n",local->thread_id,row,column);
+
+  int i;
+  for( i=0 ; i < K ; i++ ) {
+    C[row][column] += A[row][i] + B[i][column];
+  }
+
+}
+
+int main(int argc, char *argv[]) {
+
+  /* Create an array of structs. The structs hold each thread's args */
+  struct thread_args init[M*N];
+
+  /* Create an array of thread-types */
+  pthread_t threads[M*N];
+
+  int count,retval;
+  count = 0;
+  int i,j;
+  for ( i=0 ; i < M ; i++ ) {
+
+    for ( j=0 ; j < N ; j++ ) {
+
+      /* Initialize the thread's data */
+      init[count].thread_id = count;
+      init[count].row = i;
+      init[count].column = j;
+
+      /* Create the thread */
+      retval = pthread_create(&threads[i], NULL, matrixCruncher, 
+        (void *) &init[count]);
+      if (retval) {
+        printf("ERROR: pthread_create() returned %d\n",retval);
+      }
+
+      count++;
+
+    }
+
+  }
+
+  /* Wait for the threads to finish */
+  for ( i=0 ; i < M*N ; i++) {
+    pthread_join(threads[i],NULL);
+  }
+
+  /* Output the final matrix */
+  for ( i=0 ; i < M ; i++ ) {
+
+    for ( j=0 ; j < N ; j++ ) {
+
+      printf("%5d",C[i][j]);
+
+    }
+
+    printf("\n");
+
+  }
+
+}
