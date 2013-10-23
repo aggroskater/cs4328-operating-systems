b31f63009c1ca2de06e0ef1db5fefac71aec73c7
Preston Maness <aggroskater@gmail.com>
2013-10-23 16:38:42 -0500

    This ought to get me at least a few points.

--- proj1/part1b.c
+++ proj1/part1b.c
@@ -1,23 +1,24 @@
-/* This is supposed to be done on Windoze. Lol. */
-
-/* Well, I've got some time before class. Maybe I could knock this out */
-
-/* Gonna work off the class slides first. */
+/* I don't know if this compiles or not. I based this largely off the 
+ * slides for Chapter 4.
+ */
 
 #include <windows.h>
 #include <stdio.h>
 
-/* Need to make a thread args struct somehow... */
+/* Global rray to hold the fibonacci numbers */
+DWORD array[50] = {-1}; 
 
 /* threaded function */
 DWORD WINAPI fibonacci(LPVOID Param) {
 
-  /* need to figure out how to declare local struct ptr and cast Param */
-  local;
+  DWORD  limit = *(DWORD*)Param;
 
-  for (DWORD i = 2 ; i < local->size ; i++ ) {
-    local->array[i] = local->array[i-1] + local->array[i-2];
+  DWORD i;
+  for ( i = 2 ; i < limit ; i++ ) {
+    array[i] = array[i-1] + array[i-2];
   }
+
+  return 0;
 
 }
 
@@ -25,21 +26,47 @@ int main(int argc, char *argv[]) {
 
   DWORD ThreadId;
   HANDLE ThreadHandle;
-  
-  struct thread_args;
+  int Param;
 
+  /* Seed the array */
+  array[0] = 0;
+  array[1] = 1;
+
+  /* Grab user input */
+  if (argc != 2) {
+    printf("An integer parameter is required. `./part1b $n` where $n is a positive integer greater than or equal to two and less than or equal to 47.\n");
+    return -1;
+  }
+
+  Param = atoi(argv[1]);
+
+  /* Validate input */
+  if (Param < 2 || Param > 47) {
+    printf("An integer >=2 or <=47 is required.\n");
+    return -1;
+  }
+ 
+  /* Create the thread */ 
   ThreadHandle = CreateThread(
-    NULL,
-    0,
-    &thread_args,
-    0,
-    &ThreadId);
+    NULL, /* Default security stuff */
+    0, /* Default stack size */
+    fibonacci, /* thread function */
+    &Param, /* thread function parameter */
+    0, /* Default creation flags */
+    &ThreadId); /* Returns the thread identifier */
 
+  /* Wait for it to finish */
   if (ThreadHandle != NULL) {
     WaitForSingleObject(ThreadHandle,INFINITE);
     CloseHandle(ThreadHandle);
   }
 
-  /* Cycle through the array */
+  /* Cycle through the finished array */
+  DWORD i;
+  for ( i=0 ; i < size ; i++) {
+    printf("%d\n",array[i];
+  }
+
+  return 0;
 
 }
