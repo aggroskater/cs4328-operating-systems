9c05e9626bd41f0205b515717315ee42010c38a7
Preston Maness <aggroskater@gmail.com>
2013-12-02 00:39:40 -0600

    Got most of main() set up. Moving onto the threaded functions.

--- proj2/part1.c
+++ proj2/part1.c
@@ -35,6 +35,11 @@ struct thread_args {
 int buffer[BUFFER_SIZE] = {-1};
 int produced = 0;
 int consumed = 0;
+int time_to_quit = 0;
+
+pthread_mutex_t mutex;
+sem_t full;
+sem_t empty;
 
 /***************************/
 /*                         */ 
@@ -42,15 +47,24 @@ int consumed = 0;
 /*                         */
 /***************************/
 
+/* This function is the glorious, job-making producer */
 void *insert_item(void* thread_args) {
 
   struct thread_args *local;
   local = (struct thread_args *) thread_args;
 
+  /* Acquire mutex */
+
+  /* Aquire empty */
+
+  /* Release empty */
+
+  /* Release mutex */
   printf("Producing %d", local.rand);
 
 }
 
+/* This function is the filthy casual consumer */
 void *remove_item(void* thread_args) {
 
   struct thread_args *local;
@@ -68,12 +82,92 @@ void *remove_item(void* thread_args) {
 
 int main(int argc, char *argv[]) {
 
-  /* Use three semaphores: empty, full, and mutex.
+  /* Command line parameters (time to sleep is global) */
+  int time_to_sleep;
+  int num_producers;
+  int num_consumers;
+
+  if (argc != 4) {
+    printf("Usage: ./executable time_to_sleep number_of_producers number_of_consumers\n");
+    printf("All numbers should be integers.\n");
+    exit(1);
+  }
+  else {
+    time_to_sleep = atoi(argv[2]);
+    num_producers = atoi(argv[3]);
+    num_consumers = atoi(argv[4]);
+  }
+
+  /* Set up the three semaphores: empty, full, and mutex.
+   * 
+   * These are declared GLOBALLY.
    * 
    * empty - Initialize to BUFFER_SIZE
    * full - Initialize to zero
    * mutex - Initialize to zero
    *
    */
+  pthread_mutex_init(&mutex, NULL);
+  sem_init(&full, 0, 0);
+  sem_init(&empty, 0, BUFFER_SIZE);
+ 
+  /* Set up the threads and their arguments. 
+   * gotta figure out how many we need. 
+   */ 
+  int total_threads;
+  total_threads = num_producers + num_consumers;
+  pthread_t threads[total_threads];
+  struct thread_args threadArgs[total_threads];
+
+  /* Create the producers */
+  int i;
+  for (i=0 ; i < producers ; i++) {
+
+    int ret;
+    ret = pthread_create(&threads[i], NULL, insert_item, 
+      (void *) &threadArgs[i]);
+
+    if(ret) {
+      printf("Error: pthread_create() returned %d\n", ret);
+    }
+
+  }
+
+  /* Create the consumers */
+  for (i=producers ; i < total_threads ; i++) {
+
+    int ret;
+    ret = pthread_create(&threads[i], NULL, remove_item, 
+      (void *) &threadArgs[i]);
+
+    if(ret) {
+      printf("Error: pthread_create() returned %d\n", ret);
+    }
+
+  }
+
+  /* Wait for them to finish */
+  for (i=0 ; i < total_threads ; i++) {
+
+    int ret;
+    void* result;
+    ret = pthread_join(threads[i], &result);
+
+    if(ret) {
+      printf("Error: pthread_join() returned %d\n", ret);
+    }
+
+    pthread_join(threads[i],NULL);
+
+  }
+
+  /* Sleep for N seconds */
+
+  
+  /* Set global flag time_to_quit to 1. Threads should exit. */
+  time_to_quit = 1;
+
+  printf("Done.\n");
+  return 0;
 
 }
